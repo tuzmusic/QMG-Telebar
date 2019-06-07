c4b2c28ae6ff117a663aac68c220330f162c8a34
tuzmusic <tuzmusic@gmail.com>
2019-06-06 21:41:10 -0400

    create mock object for test

---
+++ __tests__/__mocks__/mock-created-object.js
@@ -0,0 +1,52 @@
+export default product = {
+  id: 1042,
+  name: "M&M",
+  slug: "mm",
+  featured: false,
+  catalog_visibility: "visible",
+  description:
+    "<p>Select the candy of your choice, then select how often you would like these delivered and additional options.\u00a0 You can enter different addresses at checkout if desired</p>\n",
+  sku: "",
+  price: "10",
+  regular_price: "10",
+  sale_price: "",
+  date_on_sale_from: null,
+  date_on_sale_from_gmt: null,
+  date_on_sale_to: null,
+  date_on_sale_to_gmt: null,
+  on_sale: false,
+  related_ids: [979, 1014, 1028, 1000, 1021],
+  categories: [{ id: 34, name: "Nutty Bars", slug: "nutty-bars" }],
+  tags: [{ id: 49, name: "Candy", slug: "candy" }],
+  images: [
+    {
+      id: 973,
+      date_created: "2019-04-21T06:08:58",
+      date_created_gmt: "2019-04-21T06:08:58",
+      date_modified: "2019-04-21T06:08:58",
+      date_modified_gmt: "2019-04-21T06:08:58",
+      src:
+        "https://telebardelivery.com/wp-content/uploads/2019/04/Telebar-MM.jpg",
+      name: "Telebar MM",
+      alt: ""
+    }
+  ],
+  attributes: [
+    {
+      id: 1,
+      name: "Chocolate",
+      position: 0,
+      visible: true,
+      variation: true,
+      options: ["Weekly"]
+    },
+    {
+      id: 2,
+      name: "Filling",
+      position: 1,
+      visible: true,
+      variation: true,
+      options: ["Cherry", "Fudge", "Nuts"]
+    }
+  ]
+};
